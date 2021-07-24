da78fd966c18bc29ff1bf800cef1e439e84982da
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-25 01:18:48 +0900

    img 경로 변경

--- src/components/Clothes.tsx
+++ src/components/Clothes.tsx
@@ -17,44 +17,44 @@ export const Clothes = ({ temperature }: TWeatherTemp) => {
   const clothesRecommend = (t: string) => {
     if (parseInt(t) > 28) {
       return (
-        <img src={require("./images/1.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/1.jpg").default} />
+        <img src={img1} />
       );
     } else if (parseInt(t) >= 22 && parseInt(t) < 28) {
       return (
-        <img src={require("./images/2.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/2.jpg").default} />
+        <img src={img2} />
       );
     } else if (parseInt(t) >= 19 && parseInt(t) < 22) {
       return (
-        <img src={require("./images/3.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/3.jpg").default} />
+        <img src={img3} />
       );
     } else if (parseInt(t) >= 17 && parseInt(t) < 19) {
       return (
-        <img src={require("./images/4.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/4.jpg").default} />
+        <img src={img4} />
       );
     } else if (parseInt(t) >= 12 && parseInt(t) < 17) {
       return (
-        <img src={require("./images/5.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/5.jpg").default} />
+        <img src={img5} />
       );
     } else if (parseInt(t) > 9 && parseInt(t) < 12) {
       return (
-        <img src={require("./images/6.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/6.jpg").default} />
+        <img src={img6} />
       );
     } else if (parseInt(t) > 5 && parseInt(t) <= 9) {
       return (
-        <img src={require("./images/7.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/7.jpg").default} />
+        <img src={img7} />
       );
     } else {
       return (
         // eslint-disable-next-line jsx-a11y/alt-text
-        <img src={require("./images/8.jpg").default} />
-        // <img src={img1} />
+        // <img src={require("./images/8.jpg").default} />
+        <img src={img8} />
       );
     }
   };
